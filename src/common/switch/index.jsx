15056bb78fff04b65df204982d120156892fd635
dhramvir73 <dharmvirkumar7321@gmail.com>
2023-01-25 22:30:41 +0530

    added prefrences component

---
+++ src/common/switch/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const Switch = () => {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider"></span>
+    </label>
+  );
+};
+
+export default Switch;
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import Switch from "../../common/switch";
 const Blog = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
   return (
-    <header>
+    <header className="relative">
       <nav className="bg-[#111827] py-[16px] px-[32px] flex justify-between items-center font-[poppins]">
         <h1 className="text-[2rem] text-[#00AAFF] p-0 font-normal text-center ">
           Veer Blog
         </h1>
-        <button className="px-[12px] py-[8px] text-white text-sm border-[1px] rounded-lg border-[#00AAFF] hover:bg-[#00aaff5d] hover:text-[#00AAFF]">
+        <button
+          onClick={handleClick}
+          className="px-[12px] py-[8px] text-white text-sm border-[1px] rounded-lg border-[#00AAFF] hover:bg-[#00aaff5d] hover:text-[#00AAFF]"
+        >
           Prefrences
         </button>
+        {toggle ? (
+          <div className="flex justify-start items-center flex-col bg-white w-[180px] h-[42px] p-2 absolute right-[2rem] top-[5rem] z-10 rounded-lg">
+            <div className="flex items-center ">
+              <span className="mr-2 font-semibold text-[#111827]">
+                Select Mode
+              </span>
+              <span>
+                <Switch />
+              </span>
+            </div>
+          </div>
+        ) : null}
       </nav>
     </header>
   );
